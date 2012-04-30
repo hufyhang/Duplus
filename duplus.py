0d0acfca1fc28541ce4096bf0f1f92a709e64cba
Feifei HANG <l.daniel.hung@gmail.com>
2012-04-30 11:21:28 +0100

    Search type: only files / only dirs / all

--- duplus.py
+++ duplus.py
@@ -6,26 +6,33 @@ import sys
 import operator
 import filesize
 
+
+ARG_FILE = ('-f', 1)
+ARG_DIR = ('-d', 2)
+ARG_ALL = 3
+
 ARG_HELP = ['--help', '-?', '-help', '-h']
 ARG_NOHIDDEN = '-nh'
-ARG_FROMSIZE = '-frm'
+ARG_FROMSIZE = '-from'
 
 
-USAGE = """Usage: python duplus.py [options] [directory=HOME_DIR]
+USAGE = """Usage: python duplus.py [options] [directory]
 Example: 
-    python duplus.py -nh -frm20.12m ~/Desktop
+    python duplus.py -nh -from20.12m ~/Desktop
 Available ptions:
     -nh : Ignore hidden files/directories.
-    -frmXX : Only show the files/directoies from a minimum size. 'XX' stands for the size, e.g. 20k.
+    -fromXX : Only show the files/directoies from a minimum size. 'XX' stands for the size, e.g. 20k.
+    -d : Only search directories.
+    -f : Only search files.
 Note:
-    If the 'directory' is not given, HOME_DIR will be the target.
+    If the 'directory' is not given, current working directory will be taken.
 For the latest version, please see:
 <URL:https://github.com/hufyhang/Duplus>"""
 
-def duplus(path=os.getenv('HOME'), fromSize='0B', nohidden=False):
-    sortPrint(iterateDict(path, nohidden), filesize.bytes(fromSize))
+def duplus(path=os.getcwd(), fromSize='0B', nohidden=False, type=ARG_ALL):
+    sortPrint(iterateDict(path, nohidden, type), filesize.bytes(fromSize))
 
-def iterateDict(path, nohidden=False):
+def iterateDict(path, nohidden=False, type=ARG_ALL):
     """iterate through a given path and return a path-size-type list"""
     if os.path.exists(path) is False:
         print 'Error: Directory does not exist.'
@@ -40,9 +47,9 @@ def iterateDict(path, nohidden=False):
             if item[0] == '.':
                 continue
         ipath = os.path.join(path, item)
-        if os.path.isfile(ipath):
+        if os.path.isfile(ipath) and (type == ARG_ALL or type == ARG_FILE[1]):
             resultList.append([item, os.path.getsize(ipath), 'f'])
-        elif os.path.isdir(ipath):
+        elif os.path.isdir(ipath) and (type == ARG_ALL or type == ARG_DIR[1]):
             resultList.append([item, calcFolderSize(ipath), 'd'])
     return resultList
 
@@ -72,6 +79,7 @@ def calcFolderSize(path):
 if __name__ == '__main__':
     flag, nh = False, False
     frm = '0B'
+    typ = 0
     for arg in sys.argv:
         # firstly, check if usage info is needed
         if arg in ARG_HELP:
@@ -84,10 +92,17 @@ if __name__ == '__main__':
             frm = arg.replace(ARG_FROMSIZE, '')
             if len(frm) == 0:
                 frm = '0B'
-
+        elif arg == ARG_FILE[0]:
+            typ += ARG_FILE[1]
+        elif arg == ARG_DIR[0]:
+            typ += ARG_DIR[1]
+    
     lastArg = sys.argv[len(sys.argv) - 1]
+    if typ == 0:
+        typ = ARG_ALL
+
     if lastArg[0] == '-' or lastArg == 'duplus.py' or lastArg == './duplus.py':
-        duplus(nohidden = nh, fromSize = frm)
+        duplus(nohidden = nh, fromSize = frm, type = typ)
     else:
-        duplus(lastArg, nohidden = nh, fromSize = frm)
+        duplus(path = lastArg, nohidden = nh, fromSize = frm, type = typ)
 
